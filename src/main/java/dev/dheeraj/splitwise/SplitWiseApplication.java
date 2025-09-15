8295aefb1b2c6c7bbffa9844be04dc1aef7eb820
Dheeraj Pali <dheerajpali761@gmail.com>
2025-09-15 09:35:28 +0000

    implemented CommandLineRunner in SplitWiseApplication to take input from commandLine

--- src/main/java/dev/dheeraj/splitwise/SplitWiseApplication.java
+++ src/main/java/dev/dheeraj/splitwise/SplitWiseApplication.java
@@ -1,12 +1,28 @@
 package dev.dheeraj.splitwise;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
 import java.util.TimeZone;
 
+//if we want something to do with commandline, we've to implement CommandLIneRunner.
 @SpringBootApplication
-public class SplitWiseApplication {
+public class SplitWiseApplication implements CommandLineRunner {
+    private Scanner scanner;
+
+    public SplitWiseApplication(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        while(true){
+            System.out.println("Please enter commands to execute");
+            String input = scanner.nextLine();
+        }
+    }
 
     public static void main(String[] args) {
         // Force JVM timezone before Spring Boot starts
